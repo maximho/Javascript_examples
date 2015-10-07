4fbbd89c37057d18d458fa651f7fa6297002ba84
Maximilianoho <maximho20@gmail.com>
2015-10-07 16:23:36 +0200

    Method performed on objects

--- objects/car.js
+++ objects/car.js
@@ -1,2 +1,12 @@
 var car = {type:"Fiat", model:500, colour:"White"};
 console.log(car.type);
+
+//The name:values pairs (in JavaScript objects) are called properties.
+
+function ride(person) {
+	return "I take " + person + " for a ride in my " + car.type;
+}
+
+console.log(ride("Marie"));
+
+//Methods are stored in properties as function definitions.
