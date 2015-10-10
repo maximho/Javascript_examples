85601cbfd27d78b48bbbbb68ebfbadfc7da82ab2
Maximilianoho <maximho20@gmail.com>
2015-10-11 00:57:15 +0200

    variables

--- scope.js
+++ scope.js
@@ -1,8 +1,13 @@
-
-function myFunction() {
+#Local Variable
+function driveOne() {
 	var car = "Volvo";
 }
-
-myFunction();
+driveOne();
 console.log("I drive with my " + typeof car);
 
+#Global Variable
+var auto = "Audi";
+function driveTwo() {
+console.log("I drive with my " + typeof auto);
+}
+driveTwo();
