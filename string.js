3b483042a3b6ba8916e4314dfd89f232a7e40cb5
Maximilianoho <maximho20@gmail.com>
2015-10-12 00:03:43 +0200

    slice()

--- string.js
+++ string.js
@@ -4,9 +4,9 @@ console.log(v);
 
 var i = v.indexOf("the");
 console.log(i);
-
 var l = v.lastIndexOf("the");
 console.log(l);
-
 var s = v.search("the");
 console.log(s);
+var c = v.slice(7,10);
+console.log(c);
