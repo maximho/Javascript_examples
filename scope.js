91031be35074ca1e13e2bcf60ff21cc55f10f869
Maximilianoho <maximho20@gmail.com>
2015-10-11 02:13:39 +0200

    automatically global

--- scope.js
+++ scope.js
@@ -1,13 +1,18 @@
-#Local Variable
 function driveOne() {
 	var car = "Volvo";
 }
 driveOne();
 console.log("I drive with my " + typeof car);
 
-#Global Variable
 var auto = "Audi";
 function driveTwo() {
 console.log("I drive with my " + typeof auto);
 }
 driveTwo();
+
+function driveThree() {
+	carro = "BMW";
+}
+driveThree();
+console.log("I drive with my " + typeof carro);
+
