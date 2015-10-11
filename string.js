7dcfca6eb2be03463a73894fbdc815a90896e864
Maximilianoho <maximho20@gmail.com>
2015-10-11 23:50:48 +0200

    search()

--- string.js
+++ string.js
@@ -2,8 +2,11 @@ var v = "We are the so called \"Vikings\" from the North.";
 
 console.log(v);
 
-var i = v.indexOf("called");
+var i = v.indexOf("the");
 console.log(i);
 
-var l = v.lastIndexOf("called");
+var l = v.lastIndexOf("the");
 console.log(l);
+
+var s = v.search("the");
+console.log(s);
