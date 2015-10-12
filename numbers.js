d3b7d9a5cfdf871ef325378dcdd463f0afa60373
Maximilianoho <maximho20@gmail.com>
2015-10-12 23:38:54 +0200

    toPrecision

--- numbers.js
+++ numbers.js
@@ -3,5 +3,6 @@ var y = 123e-5; console.log(y);
 var z = 123;
 var s = z.toString(); console.log(s); console.log(typeof s); 
 var e = z.toExponential(2); console.log(e);
-var n = 9.656; var f = n.toFixed(2); console.log(f); 
+var n = 9.656; var f = n.toFixed(2); console.log(f); console.log(typeof f); 
+               var p = n.toPrecision(); console.log(p); console.log(typeof p); 
 
