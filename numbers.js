20e5deeb9553ecc09016b1b02c057957835a71db
Maximilianoho <maximho20@gmail.com>
2015-10-12 19:18:41 +0200

    toExponential

--- numbers.js
+++ numbers.js
@@ -1,5 +1,6 @@
 var x = 123e5; console.log(x);
 var y = 123e-5; console.log(y);
 var z = 123;
-var s = z.toString(); console.log(s);
+var s = z.toString(); console.log(s); console.log(typeof s); 
+var e = z.toExponential(); console.log(e);
 
