f5f8247b469077d7bbb0aac444ec4522d6deb972
Maximilianoho <maximho20@gmail.com>
2015-10-12 02:21:14 +0200

    concat

--- string.js
+++ string.js
@@ -1,18 +1,14 @@
-var v = "We are the so called \"Vikings\" from the North.";
+var v = "We are the so called \"Vikings\" from the North";
 console.log(v);
 
-var i = v.indexOf("the");
-console.log(i);
-var l = v.lastIndexOf("the");
-console.log(l);
-var s = v.search("the");
-console.log(s);
-var c = v.slice(7,10);
-console.log(c);
-var u = v.substr(7,3);
-console.log(u);
-var r = v.replace("North","South");
-console.log(r);
-var p = v.toUpperCase();
-console.log(p);
+var i = v.indexOf("the"); console.log(i);
+var l = v.lastIndexOf("the"); console.log(l);
+var s = v.search("the"); console.log(s);
+var c = v.slice(7,10); console.log(c);
+var u = v.substr(7,3); console.log(u);
+var r = v.replace("North","South"); console.log(r);
+var p = v.toUpperCase(); console.log(p); 
+var o = v.toLowerCase(); console.log(o); 
+var v2 = "of the World"
+var n = v.concat(" ",v2); console.log(n); 
 
