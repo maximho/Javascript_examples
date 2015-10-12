8c5fe47dca045b1e84ab73e43fae9ed011f25a10
Maximilianoho <maximho20@gmail.com>
2015-10-12 17:32:12 +0200

    Replace

--- 2_string.js
+++ 2_string.js
@@ -1,6 +1,8 @@
 var str1 = "Hello ";
 var str2 = "World!";
+var str3 = "Hello World!";
 var c = str1.concat(str2); console.log(c);
 var l = str1.toLowerCase(); console.log(l);
 var u = str1.toUpperCase(); console.log(u);
+var r = str3.replace("World","Universe");console.log(r);
 
