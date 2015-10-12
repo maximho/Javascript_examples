8e9fbc1359f528e5ff99c71492471dcce186a0d3
Maximilianoho <maximho20@gmail.com>
2015-10-12 19:30:57 +0200

    toFixed

--- numbers.js
+++ numbers.js
@@ -2,5 +2,6 @@ var x = 123e5; console.log(x);
 var y = 123e-5; console.log(y);
 var z = 123;
 var s = z.toString(); console.log(s); console.log(typeof s); 
-var e = z.toExponential(); console.log(e);
+var e = z.toExponential(2); console.log(e);
+var n = 9.656; var f = n.toFixed(2); console.log(f); 
 
