55a332529a235f5752f7330959e40a988ca4757b
Maximilianoho <maximho20@gmail.com>
2015-10-13 12:28:42 +0200

    Number()

--- numbers.js
+++ numbers.js
@@ -3,6 +3,7 @@ var y = 123e-5; console.log(y);
 var z = 123;
 var s = z.toString(); console.log(s); console.log(typeof s); 
 var e = z.toExponential(2); console.log(e);
-var n = 9.656; var f = n.toFixed(2); console.log(f); console.log(typeof f); 
-               var p = n.toPrecision(); console.log(p); console.log(typeof p); 
-
+var n = 9.656; var f = n.toFixed(2); console.log(f); console.log(typeof f);  
+var p = n.toPrecision(); console.log(p); console.log(typeof p); 
+console.log(Number(true)); console.log(Number(false)); 
+console.log(Number("10")); console.log(Number("10 20"));
