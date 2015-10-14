ca84a59e26f74212ca5017c9b1f463c2eb1617cc
Maximilianoho <maximho20@gmail.com>
2015-10-14 18:51:51 +0200

    .ceil()/.floor()

--- math.js
+++ math.js
@@ -2,4 +2,5 @@ console.log(Math.random());
 console.log(Math.min(4, 45, -5, -43, 254)); 
 console.log(Math.max(4, 45, -5, -43, 254));
 console.log(Math.round(4.7)); console.log(Math.round(4.3));
+console.log(Math.ceil(4.3)); console.log(Math.floor(4.7));
 
