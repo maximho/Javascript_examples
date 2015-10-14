a6194ff247e56b8b166cf2e125689a50af235330
Maximilianoho <maximho20@gmail.com>
2015-10-14 20:37:50 +0200

    Creating Date Objects

--- dates.js
+++ dates.js
@@ -1,1 +1,5 @@
-console.log(Date())
+console.log(Date()) // Displaying date
+console.log(new Date()) // Creating Date Objects
+// console.log(new Date(milliseconds)) // Creating Date Objects
+// console.log(new Date(dateString)) // Creating Date Objects
+// console.log(new Date(year, month, day, hours, minutes, seconds, milliseconds)) // Creating Date Objects
