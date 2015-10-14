4f125b629093a7d00a9bcc1a360075299361375e
Maximilianoho <maximho20@gmail.com>
2015-10-14 19:25:41 +0200

    Square root of 2 - of 1/2

--- math.js
+++ math.js
@@ -4,4 +4,5 @@ console.log(Math.max(4, 45, -5, -43, 254));
 console.log(Math.round(4.7)); console.log(Math.round(4.3));
 console.log(Math.ceil(4.3)); console.log(Math.floor(4.7));
 console.log(Math.floor(Math.random() * 11)); // random number between 0 and 10
-console.log(Math.E); console.log(Math.PI);
+console.log(Math.E); console.log(Math.PI); // Euler's number and PI
+console.log(Math.SQRT2); console.log(Math.SQRT1_2); // Square root of 2 - of 1/2 
