c24c64263d7344682abaa998a513caf8526ed357
Maximilianoho <maximho20@gmail.com>
2015-10-14 19:38:46 +0200

    Natural logarithm of 2 and 10

--- math.js
+++ math.js
@@ -6,3 +6,4 @@ console.log(Math.ceil(4.3)); console.log(Math.floor(4.7));
 console.log(Math.floor(Math.random() * 11)); // random number between 0 and 10
 console.log(Math.E); console.log(Math.PI); // Euler's number and PI
 console.log(Math.SQRT2); console.log(Math.SQRT1_2); // Square root of 2 - of 1/2 
+console.log(Math.LN2); console.log(Math.LN10); // Natural logarithm of 2 and 10 
