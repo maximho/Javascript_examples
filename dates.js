82ca280977750004c3106f88d81806e30527ff8f
Maximilianoho <maximho20@gmail.com>
2015-10-14 20:50:21 +0200

    new date object as zero time plus the number

--- dates.js
+++ dates.js
@@ -4,3 +4,4 @@ console.log(new Date()) // Creating Date Objects
 // console.log(new Date(dateString)) // Creating Date Objects
 // console.log(new Date(year, month, day, hours, minutes, seconds, milliseconds)) // Creating Date Objects
 var d = new Date("October 14, 2015 20:44:00"); console.log(d);
+var a = new Date(86400000); console.log(a);
