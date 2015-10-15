5ec3724bb6ac04d88dcf2c6a1bda11b630e0f0ad
Maximilianoho <maximho20@gmail.com>
2015-10-15 17:12:39 +0200

    add dates to a date

--- dates.js
+++ dates.js
@@ -18,3 +18,4 @@ console.log(f.getDay()); // weekday as a number (0-6)
 var days = ["Sunday","Monday","Tuesday","Wednesday","Thursday","Friday","Saturday"]; console.log(days[f.getDay()]); 
 console.log(f.setFullYear(2020, 0, 14)); // 
 console.log(f.setDate(20)); // 
+console.log(f.setDate(f.getDate() + 50)); // 
