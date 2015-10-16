e5e41f35120514266ef4df878b2ca713c19c357d
Maximilianoho <maximho20@gmail.com>
2015-10-16 15:36:18 +0200

    loop array

--- arrays.js
+++ arrays.js
@@ -5,15 +5,25 @@ var autos = new Array("Opel", "Peugot", "Fiat");
 console.log(autos);
 autos[0] = "Renault";
 console.log(autos);
+
 var person = {firstName:"John", age:34, eyesColour:"brown"}; console.log(person);
+
 console.log(cars.length);
 console.log(cars.sort());
+
 cars.push("Ford"); console.log(cars);
-
-
-
 cars[cars.length] = "Torino"; console.log(cars);
 
 
 
 
+
+
+
+var index;
+var text = "";
+var fruits = ["Banana", "Apple", "Orange"];
+for (index = 0; index < fruits.length; index++) {
+  text += fruits[index] + " ";
+}
+console.log(text);
